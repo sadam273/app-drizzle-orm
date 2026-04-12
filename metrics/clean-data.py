519046f460e60761b03be4035b1016f3fecdab54
Sadam <msadamramli@gmail.com>
2026-04-12 13:37:35 +0700

    benerin path clean-data.py

--- metrics/clean-data.py
+++ metrics/clean-data.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import os
 
-df = pd.read_csv('../response_time_result.csv')
+base_dir = os.path.dirname(os.path.abspath(__file__))
+csv_input_path = os.path.join(base_dir, "..", "response_time_result.csv")
+csv_output_path = os.path.join(base_dir, "..", "response_time_clean.csv")
 
-# ambil hanya response time per request
+df = pd.read_csv(csv_input_path)
+
 duration = df[df['metric_name'] == 'http_req_duration'][['timestamp', 'metric_value']]
 duration.columns = ['timestamp', 'response_time_ms']
 
-duration.to_csv('response_time_clean.csv', index=False)
+duration.to_csv(csv_output_path, index=False)
 print(duration.describe())
