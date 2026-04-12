a02493478245db8c6bff3a55f06294149aff8cc9
Sadam <msadamramli@gmail.com>
2026-04-12 12:57:43 +0700

    tambahin di subprocess biar ngebentukin response time

--- metrics/run-test.py
+++ metrics/run-test.py
@@ -3,22 +3,19 @@ import datetime
 import time
 import os
 
-# Cari lokasi absolut folder 'metrics' tempat script ini berada, lalu arahkan ke 'k6/read.js'
 base_dir = os.path.dirname(os.path.abspath(__file__))
 k6_script_path = os.path.join(base_dir, "..", "k6", "read.js")
+csv_output_path = os.path.join(base_dir, "response_time_result.csv")
 
-# Catat waktu start
 start_time = datetime.datetime.utcnow()
 print(f"Test started at: {start_time.isoformat()}Z")
 
-# Jalankan k6 (sekarang jalurnya sudah anti-nyasar)
-subprocess.run(["k6", "run", k6_script_path])
+# Tambah --out csv di sini
+subprocess.run(["k6", "run", "--out", f"csv={csv_output_path}", k6_script_path])
 
-# Catat waktu end
 end_time = datetime.datetime.utcnow()
 print(f"Test ended at: {end_time.isoformat()}Z")
 
-# Simpan ke file
 with open("test_time.txt", "w") as f:
     f.write(f"start={start_time.strftime('%Y-%m-%dT%H:%M:%SZ')}\n")
     f.write(f"end={end_time.strftime('%Y-%m-%dT%H:%M:%SZ')}\n")
